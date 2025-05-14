14c5fb8a964f7e1d5fc18462f506991642811e39
Bhaskarjyoti Roy <bhaskarjyoti@gmail.com>
2025-05-14 19:43:31 +0530

    backup script fix for S3 endpoint

--- backup.sh
+++ backup.sh
@@ -11,6 +11,7 @@ MONGO_USER=${MONGO_USER:-admin}
 MONGO_PASSWORD=${MONGO_PASSWORD:-password}
 S3_BUCKET=${S3_BUCKET:-backup-bucket}
 S3_BACKUP_PATH=${S3_BACKUP_PATH:-mongodb-backups}
+S3_ENDPOINT=${S3_ENDPOINT:-"https://s3.amazonaws.com"}
 
 # Set default retention period if not defined
 RETENTION_PERIOD=${RETENTION_PERIOD:-30}
@@ -35,22 +36,22 @@ BACKUP_ARCHIVE="/tmp/mongo_backup_$DATE.tar.gz"
 tar -czf "$BACKUP_ARCHIVE" -C "$BACKUP_DIR" .
 
 # Ensure the S3 backup path exists
-aws s3api head-object --bucket "$S3_BUCKET" --key "$S3_BACKUP_PATH/" 2>/dev/null || aws s3api put-object --bucket "$S3_BUCKET" --key "$S3_BACKUP_PATH/"
+aws s3api head-object --bucket "$S3_BUCKET" --key "$S3_BACKUP_PATH/" --endpoint-url "$S3_ENDPOINT" 2>/dev/null || aws s3api put-object --bucket "$S3_BUCKET" --key "$S3_BACKUP_PATH/" --endpoint-url "$S3_ENDPOINT"
 
 # Update the backup path to include the directory
 BACKUP_PATH="$S3_BACKUP_PATH/mongo_backup_$DATE.tar.gz"
 
 # Upload to S3
-aws s3 cp "$BACKUP_ARCHIVE" "s3://$S3_BUCKET/$BACKUP_PATH" --storage-class STANDARD
+aws s3 cp "$BACKUP_ARCHIVE" "s3://$S3_BUCKET/$BACKUP_PATH" --storage-class STANDARD --endpoint-url "$S3_ENDPOINT"
 
 # Clean up local backups
 rm -rf "$BACKUP_DIR"
 rm -f "$BACKUP_ARCHIVE"
 
 # Delete old backups from S3
-aws s3 ls "s3://$S3_BUCKET/$S3_BACKUP_PATH/" | awk '{print $4}' | while read -r FILE; do
+aws s3 ls "s3://$S3_BUCKET/$S3_BACKUP_PATH/" --endpoint-url "$S3_ENDPOINT" | awk '{print $4}' | while read -r FILE; do
     FILE_DATE=$(echo "$FILE" | grep -oE "[0-9]{4}-[0-9]{2}-[0-9]{2}")
     if [[ "$FILE_DATE" < $(date -d "$RETENTION_PERIOD days ago" +"%Y-%m-%d") ]]; then
-        aws s3 rm "s3://$S3_BUCKET/$S3_BACKUP_PATH/$FILE"
+        aws s3 rm "s3://$S3_BUCKET/$S3_BACKUP_PATH/$FILE" --endpoint-url "$S3_ENDPOINT"
     fi
 done
